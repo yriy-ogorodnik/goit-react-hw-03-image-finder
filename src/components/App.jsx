43b91ad2cc77749b025f62f951699e6cb37a12ce
Yriy Ogorodnik <yriyelektric@gmail.com>
2023-03-31 23:55:24 +0300

    121

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,35 @@
-export const App = () => {
-  return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
-  );
-};
+import { Component } from 'react';
+import Searchbar from './Searchbar/Searchbar';
+import * as API from './Api/Api';
+import {ImageGallery} from './ImageGallery/ImageGallery';
+export class App extends Component {
+  state = {
+    searchText: '',
+    images: [],
+  };
+
+  // викликаємо по кліку на пошук. перезаписуємо стейт
+  createSearchText = searchText => {
+    this.setState({ searchText });
+  };
+
+  // метод, который вызывается при обновлении и перерисовке компоненты.
+  // Цей метод не викликається під час першого рендеру
+  componentDidUpdate(prevProps, prevState) {
+    const searchText = this.state.searchText.trim()
+    if (prevState.searchText !== searchText && searchText) {
+      API.fetchImages(searchText).then(({ hits,totalHits }) => {
+        this.setState({images:hits})
+      });
+    }
+  }
+
+  render() {
+    return (
+      <div className='App'>
+        <Searchbar createSearchText={this.createSearchText} />
+        <ImageGallery images={this.state.images}/>
+      </div>
+    );
+  }
+}
---
+++ src/components/Searchbar/Searchbar.jsx
@@ -0,0 +1,41 @@
+import { Component } from 'react';
+
+class Searchbar extends Component {
+  state = {
+    value: '',
+  };
+
+  handlChange = ({ target: { value } }) => {
+    this.setState({ value});
+  };
+
+  handlSubmit = e => {
+    e.preventDefault();
+    this.props.createSearchText(this.state.value)
+    
+  };
+
+  render() {
+    return (
+      <header className="Searchbar">
+        <form className="SearchForm" onSubmit={this.handlSubmit}>
+          <button type="submit" className="SearchForm-button">
+            <span className="SearchForm--label">Search</span>
+          </button>
+
+          <input
+            onChange={this.handlChange}
+            className="SearchForm-input"
+            type="search"
+            autoComplete="off"
+            autoFocus
+            placeholder="Search images and photos"
+            value={this.state.value}
+          />
+        </form>
+      </header>
+    );
+  }
+}
+
+export default Searchbar;
